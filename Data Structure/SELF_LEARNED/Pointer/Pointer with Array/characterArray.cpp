b192e11f2dcc11f7c4be00724165eb751aa4a4cb
ZUIREZ <rashedxshawon@gmail.com>
2024-10-09 10:24:21 +0600

    new file:   Data Structure/SELF_LEARNED/Pointer/Pointer with Array/characterArray.cpp

---
+++ Data Structure/SELF_LEARNED/Pointer/Pointer with Array/characterArray.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+    char ch[7] = "Rashed";
+
+    cout<<"Printing arr : "<<arr<<endl;
+    cout<<"Printing ch : "<<ch<<endl;
+}
