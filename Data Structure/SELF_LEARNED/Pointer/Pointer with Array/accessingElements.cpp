da77a895590e8ea8e44345ef330adf462453dfd3
ZUIREZ <rashedxshawon@gmail.com>
2024-10-09 10:20:44 +0600

    new file:   Data Structure/SELF_LEARNED/Pointer/Pointer with Array/accessingElements.cpp

---
+++ Data Structure/SELF_LEARNED/Pointer/Pointer with Array/accessingElements.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10] = {10,11,12,13,14,15,16,17,18,19};
+
+    //Accessing the value at index 2
+    cout<<"Value at index 2 : "<<arr[2]<<endl;
+    cout<<"Value at index 2  : "<<*arr+2<<endl;
+}
