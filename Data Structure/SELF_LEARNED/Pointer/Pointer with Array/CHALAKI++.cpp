6fd42315913bf5018309c3740830f3cf5cac3f18
ZUIREZ <rashedxshawon@gmail.com>
2024-10-09 10:23:24 +0600

    new file:   Data Structure/SELF_LEARNED/Pointer/Pointer with Array/CHALAKI++.cpp

---
+++ Data Structure/SELF_LEARNED/Pointer/Pointer with Array/CHALAKI++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10] = {10,11,12,13,14,15,16,17,18,19};
+
+    cout<<"Value at index 2 : "<<2[arr]<<endl;
+
+    //equivalent statement
+    cout<<"Value at index 2  : "<<*arr+2<<endl;
+}
