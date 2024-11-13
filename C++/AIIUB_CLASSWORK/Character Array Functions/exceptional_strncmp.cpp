b837de8b02a3c3b6b5adbf8fccd7a46c367e0fca
ZUIREZ <rashedxshawon@gmail.com>
2024-11-13 20:24:55 +0600

    Add exceptional strncmp example for ASCII comparison

---
+++ C++/AIIUB_CLASSWORK/Character Array Functions/exceptional_strncmp.cpp
@@ -0,0 +1,19 @@
+/*
+
+nomally strncmp show 1,-1,0 when s1 is greater, less or equal
+But if we select the value 1 to compare the first words of the strings
+it will show the ascii difference too.
+
+*/
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    char S1[50] = "ABCDZ";
+    char S2[50] = "DBEDX";
+    cout << "Exceptional Compare:" << strncmp(S1, S2, 1) << endl;
+    cout << "Exceptional Compare:" << strncmp(S2, S1, 1) << endl;
+}
