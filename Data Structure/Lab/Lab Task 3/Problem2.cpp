136e3c5220f57bfb87a261c4094ce33b5bcea955
zuirez <rashedxshawon@gmail.com>
2025-03-16 09:29:55 +0600

    Refactor variable names for clarity in Problem2 implementation

--- Data Structure/Lab/Lab Task 3/Problem2.cpp
+++ Data Structure/Lab/Lab Task 3/Problem2.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main()
 {
     int size;
-    bool a=false;
+    bool duplicateElement=false;
 
     cout<<"Enter the size of array : ";
     cin>>size;
 
     int arr[size];
 
+    cout<<"Enter the elements of array : ";
     for(int i=0; i<size; i++)
     {
         cin>>arr[i];
@@ -22,7 +23,7 @@ int main()
         {
             if(arr[i]==arr[j])
             {
-                a=true;
+                duplicateElement=true;
                 for(int k=j; k<size-1; k++)
                 {
                     arr[k]=arr[k+1];
@@ -33,7 +34,7 @@ int main()
         }
     }
 
-    if(a==false)
+    if(duplicateElement==false)
     {
         cout<<"Array already unique.";
     }
