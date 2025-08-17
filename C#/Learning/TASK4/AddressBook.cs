8815c95593a35ad75b32417cf28449776e127568
zuirez <rashedxshawon@gmail.com>
2025-08-18 00:54:06 +0600

    Refactor AddressBook class to improve structure and add properties

--- C#/Learning/TASK4/AddressBook.cs
+++ C#/Learning/TASK4/AddressBook.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TASK4
-{
-    internal class AddressBook
+﻿public class AddressBook
     {
+        private string ownerName;
+        private string info;
+        private Contact[] listOfContact;
+        private int totalContact;
+
+        // Properties
+        public string OwnerName { get => ownerName; set => ownerName = value; }
+        public string Info { get => info; set => info = value; }
+        public Contact[] ListOfContact { get => listOfContact; set => listOfContact = value; }
+        public int TotalContact { get => totalContact; set => totalContact = value; }
+
+        // Constructors
+        public AddressBook()
+        {
+            listOfContact = new Contact[100];
+            totalContact = 0;
+        }
+        public AddressBook(string owner, string info)
+        {
+            this.ownerName = owner;
+            this.info = info;
+            listOfContact = new Contact[100];
+            totalContact = 0;
+        }
+
+        // Methods
+        public void ShowAllContactInfo()
+        {
+            Console.WriteLine($"AddressBook Owner: {ownerName}, Info: {info}, Total Contacts: {totalContact}");
+            for (int i = 0; i < totalContact; i++)
+            {
+                listOfContact[i].ShowPersonInfo();
+            }
+        }
+
+        public void AddContact(Contact con)
+        {
+            listOfContact[totalContact++] = con;
+            Console.WriteLine("Contact added.");
+        }
+
+        public void DeleteContact(Contact con)
+        {
+            for (int i = 0; i < totalContact; i++)
+            {
+                if (listOfContact[i] == con)
+                {
+                    for (int j = i; j < totalContact - 1; j++)
+                        listOfContact[j] = listOfContact[j + 1];
+                    totalContact--;
+                    Console.WriteLine("Contact deleted.");
+                    return;
+                }
+            }
+        }
     }
 }
